60bef7dded876c0e87318a88588dbdfeb27f2ba4
frapee-pugal <frapee-pugal@Murali>
2025-02-20 13:00:45 +0000

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## promotor
+
+none
+
+#### License
+
+MIT
---
+++ promotor/config/desktop.py
@@ -0,0 +1,10 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "promotor",
+			"type": "module",
+			"label": _("promotor")
+		}
+	]

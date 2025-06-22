07968a873881f9ac3f7ef3a10dfde3c64b63cbaa
Pugal <pugalenthipugalenthi09686@gmail.com>
2025-06-22 07:18:07 +0000

    Bulk Convertion Update

---
+++ promotor/promotor/bulk_convertion.py
@@ -0,0 +1,42 @@
+import requests
+import frappe
+from frappe.model.mapper import get_mapped_doc
+from frappe.model.document import Document
+
+@frappe.whitelist()
+def make_bulk_convertion(source_name, target_doc=None):
+	return _make_bulk_convertion(source_name, target_doc)
+
+
+def _make_bulk_convertion(source_name, target_doc=None, ignore_permissions=False):
+	def set_missing_values(source, target):
+		if source.company_name:
+			target.customer_type = "Company"
+			target.customer_name = source.company_name
+		else:
+			target.customer_type = "Individual"
+			target.customer_name = source.lead_name
+
+		target.customer_group = frappe.db.get_default("Customer Group")
+
+	doclist = get_mapped_doc(
+		"Lead",
+		source_name,
+		{
+			"Lead": {
+				"doctype": "Customer",
+				"field_map": {
+					"name": "lead_name",
+					"company_name": "customer_name",
+					"contact_no": "phone_1",
+					"fax": "fax_1",
+				},
+				"field_no_map": ["disabled"],
+			}
+		},
+		target_doc,
+		set_missing_values,
+		ignore_permissions=ignore_permissions,
+	)
+
+	return doclist
